0268dbf8a1906eb9ed88f8ab3fbacf671c96a270
unknown <christy.g.lee@gmail.com>
2020-10-09 09:36:26 -0700

    added npm axios install for github profile image

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const generateMarkdown = require("./generateMarkdown");
+const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
