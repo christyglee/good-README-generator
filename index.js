dc07600a0e8c92918c87acdb3eea5331c260485b
unknown <christy.g.lee@gmail.com>
2020-10-08 20:16:47 -0700

    first prompt generated for Title

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const questions = [
+// const questions = [
 
-];
+// ];
 
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
-function init() {
+// function init() {
 
-}
+// }
 
-init();
+// init();
 
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
-const writeToFile = util.promisify(fs.writeFile);
+// const writeToFile = util.promisify(fs.writeFile);
 
+function userPrompt() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+        }
+        
+        
+    ])
+} 
+
+userPrompt();
